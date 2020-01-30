d17df6f9c9933fd270c21aae604f86c9e88da69e
Jesse-Tingle <jtingle0@gmail.com>
2020-01-30 18:01:22 -0500

    fix error in postActions file

--- giga-pet/src/components/Profiles/ChildProfile.jsx
+++ giga-pet/src/components/Profiles/ChildProfile.jsx
@@ -1,15 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { Route, Link } from 'react-router-dom';
-import FoodForm from '../FoodList/FoodForm';
+import React, { useState, useEffect } from "react";
+import { Route, Link } from "react-router-dom";
+import FoodForm from "../FoodList/FoodForm";
+import Child from "../Users/Child";
 
 function ChildProfile() {
-    return (
-        <div className="wrapper">
-            <Route exact path="/add-food" ></Route>
+  return (
+    <div className="wrapper">
+      <Child />
 
-            <Link to="/add-food" component={FoodForm}>Submit</Link>
-        </div>
-    )
+      <Route exact path="/add-food"></Route>
+      <Link to="/add-food" component={FoodForm}>
+        Submit
+      </Link>
+    </div>
+  );
 }
 
 export default ChildProfile;
--- giga-pet/src/redux/actions/postActions.js
+++ giga-pet/src/redux/actions/postActions.js
@@ -1,8 +1,8 @@
-export const fetchFoodPosts = () => {
+export const fetchFoodPosts = () => dispatch => {
   api()
     .get(/* put api link here */)
     .then(res =>
-      dispatchEvent({
+      dispatch({
         type: FETCH_FOOD_POSTS,
         payload: res.data
       })
