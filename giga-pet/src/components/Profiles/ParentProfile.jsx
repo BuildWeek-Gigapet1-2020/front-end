dfc2685f9e86d2db95ba4b5c74d32996d973687e
elijahmandeville <elijahmandeville@gmail.com>
2020-01-29 19:13:38 -0700

    added dependencies

---
+++ giga-pet/src/components/Profiles/ParentProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ParentProfile() {
+    return (
+        <div>
+            <h1>ParentProfile Content</h1>
+        </div>
+    )
+}
+
+export default ParentProfile;
---
+++ giga-pet/src/components/Users/Monster.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Monster() {
+    return (
+        <div>
+            <h1>Monster Content</h1>
+        </div>
+    )
+}
+
+export default Monster;
