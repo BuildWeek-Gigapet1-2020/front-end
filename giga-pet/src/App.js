adf275ac5fb6dc458d9451d5efa3dc85757ffb20
Jesse-Tingle <jtingle0@gmail.com>
2020-01-30 02:00:51 -0500

    set up withRouter on App.js

--- giga-pet/src/App.js
+++ giga-pet/src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import './App.css';
-import Login from './components/Authentication/Login';
-import SignUp from './components/Authentication/SignUp';
-import { Route, withRouter, Link } from 'react-router-dom';
-
+import React from "react";
+import "./App.css";
+import Login from "./components/Authentication/Login";
+import SignUp from "./components/Authentication/SignUp";
+import { Route, withRouter, Link } from "react-router-dom";
 
 function App() {
-
   return (
     <div className="wrapper">
-      <Link to='/login'>Log In</Link>
-      <Link to='/signup'>Sign Up</Link>
+      <Link to="/login">Log In</Link>
+      <Link to="/signup">Sign Up</Link>
 
       <div className="App">
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/signup' component={SignUp} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
       </div>
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
