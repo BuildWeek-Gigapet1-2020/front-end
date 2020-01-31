ee79cd84491039574195c7221c25191afed80194
Jesse-Tingle <jtingle0@gmail.com>
2020-01-30 20:24:37 -0500

    create types and userActions for Login and Register

--- giga-pet/src/redux/actions/types.js
+++ giga-pet/src/redux/actions/types.js
@@ -1,2 +1,7 @@
 export const FETCH_FOOD_POSTS = "FETCH_FOOD_POSTS";
 export const NEW_FOOD_POST = "NEW_FOOD_POST";
+
+export const LOADING = "LOADING";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_ERROR = "ERROR";
+export const NEW_USER_SUCCESS = "NEW_USER_SUCCESS";
