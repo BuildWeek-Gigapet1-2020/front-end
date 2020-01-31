41b6a40852b12bfc97395ccf7073540e53baa3b2
Jesse-Tingle <jtingle0@gmail.com>
2020-01-30 21:21:31 -0500

    remove comments

--- giga-pet/src/components/Authentication/Signup.jsx
+++ giga-pet/src/components/Authentication/Signup.jsx
@@ -1,20 +1,3 @@
-// import React from "react";
-
-// function Signup() {
-//   return (
-//     <form>
-//       <input name="parent-name" placeholder="Parent Name" type="text" />
-//       <input name="username" placeholder="Username" type="text" />
-//       <input name="email" placeholder="Email" type="text" />
-//       <input name="password" placeholder="Password" type="password" />
-
-//       <button type="submit">Submit</button>
-//     </form>
-//   );
-// }
-
-// export default Signup;
-
 import React, { useState } from "react";
 // import api from '../utils/api';
 
