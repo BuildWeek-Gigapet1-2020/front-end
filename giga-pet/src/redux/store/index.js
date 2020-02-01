79589b1c2e0ae20c30b012ed2bbd945382a29359
Jesse-Tingle <jtingle0@gmail.com>
2020-02-01 02:39:23 -0500

    delet foodReducer.js file -> unnessessary file

--- giga-pet/src/redux/store/index.js
+++ giga-pet/src/redux/store/index.js
@@ -2,22 +2,16 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-import { foodReducer } from "../reducer/foodReducer";
 import { userReducer } from "../reducer/userReducer";
-
-// const initialState = {};
 
 const middleware = [thunk];
 
 const rootReducer = combineReducers({
-  foodReducer,
   userReducer
 });
 
 const store = createStore(
   rootReducer,
-
-  // initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
