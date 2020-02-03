03c43e08a004dfe1763309277e2e8d9345b23013
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2020-02-02 20:03:51 -0500

    Set up food form

--- giga-pet/src/components/forms/FoodForm.jsx
+++ giga-pet/src/components/forms/FoodForm.jsx
@@ -1,26 +1,37 @@
-import React from "react";
-// import api from "../utils/api";
+import React, { useEffect, useState } from "react";
+import api from "../utils/api";
 
 function FoodForm() {
-  //   useEffect(() => {
-  //     api()
-  //       .get("/api/")
-  //       .then()
-  //       .catch();
-  //   }, []);
+  const [food, setFood] = useState({
+    name: "",
+    type: "",
+    servings: 0
+  })
+
+  const handleChange = event => {
+    setFood({ ...food, [event.target.food]: event.target.value })
+  }
+
+  const handleSubmit = event => {
+    api()
+        .POST("/api/foods")
+        .then( res => (console.log(res.data)) )
+        .catch(error => console.log("Error", error));
+  }
+
 
   // create onChange for form below
-
+    
   // create onSubmit for form below
 
   // add form validation
   //    - for food name make sure the text typed in is a string and not a number
 
   return (
-    <form>
-      <input type="text" name="food-name" placeholder="Food Name" />
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="name" placeholder="Food Name" value={food.name} onChange={handleChange}/>
 
-      <select name="food-type">
+      <select name="type" value={food.type} onChange={handleChange}>
         <option value="fruit"></option>
         <option value="vegetable"></option>
         <option value="whole-grains"></option>
