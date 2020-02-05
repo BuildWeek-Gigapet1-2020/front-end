41ef03bf69881ce4c4d19aec95c64811e813e4f2
Jesse-Tingle <jtingle0@gmail.com>
2020-02-05 16:59:43 -0500

    add edit and delete buttons to Food component

--- giga-pet/src/components/FoodList/Food.jsx
+++ giga-pet/src/components/FoodList/Food.jsx
@@ -1,14 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import styled from "styled-components";
 
 function Food(props) {
   console.log("Food component props", props);
   return (
-    <div>
+    <ContainerDiv>
+      <div className="edit-delete">
+        <Link to={{ pathname: `/` }}>
+          <button className="btn-left">
+            <p>Edit</p>
+          </button>
+        </Link>
+
+        <button className="btn-right">
+          <p>Delete</p>
+        </button>
+      </div>
       <h1>Food Name: {props.food.name}</h1>
       <h2>Food Type: {props.food.type}</h2>
       <h3>Serving Size: {props.food.servings}</h3>
-    </div>
+    </ContainerDiv>
   );
 }
 
 export default Food;
+
+const ContainerDiv = styled.div`
+  .edit-delete {
+    display: flex;
+    justify-content: center;
+    margin-top: 5%;
+
+    .btn-left {
+      margin-right: 5%;
+    }
+  }
+`;
