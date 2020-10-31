e772bab46487685ed710c695d28bc916087ab015
Jesse-Tingle <jtingle0@gmail.com>
2020-10-31 19:18:10 -0400

    updates to styling on forms pages to make app look more polished

--- giga-pet/src/components/FoodList/Food.jsx
+++ giga-pet/src/components/FoodList/Food.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from "react"
+import { Link } from "react-router-dom"
 
-import styled from "styled-components";
+import styled from "styled-components"
 
 function Food(props) {
-  // console.log("Food component props", props);
-  return (
-    <ContainerDiv>
-      <div className="edit-delete">
-        <Link
+	// console.log("Food component props", props);
+	return (
+		<ContainerDiv>
+			<div className="edit-delete">
+				{/* <Link
           to={{
             pathname: `/edit-food/${props.food.name}`,
             state: {
@@ -25,21 +25,21 @@ function Food(props) {
           <button className="btn-left">
             <p>Edit</p>
           </button>
-        </Link>
+        </Link> */}
 
-        <button className="btn-right">
-          <p>Delete</p>
-        </button>
-      </div>
-      {/* <h4>food id: {props.food.id}</h4> */}
-      <h1>Food Name: {props.food.name}</h1>
-      <h2>Food Type: {props.food.type}</h2>
-      <h3>Serving Size: {props.food.servings}</h3>
-    </ContainerDiv>
-  );
+				<button className="btn-right">
+					<p>Delete</p>
+				</button>
+			</div>
+			{/* <h4>food id: {props.food.id}</h4> */}
+			<h1>Food Name: {props.food.name}</h1>
+			<h2>Food Type: {props.food.type}</h2>
+			<h3>Serving Size: {props.food.servings}</h3>
+		</ContainerDiv>
+	)
 }
 
-export default Food;
+export default Food
 
 const ContainerDiv = styled.div`
 
@@ -66,6 +66,6 @@ const ContainerDiv = styled.div`
       margin: 0px;
     }
   }
-`;
+`
 
 // to={{ pathname: `/edit-food/${props.id}`, state: {id: props.id, name: props.name, created_at: props.created_at, updated_at: props.updated_at, child_id: props.child_id, type: props.type, servings: props.servings}  }}
